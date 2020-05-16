ef33e1dd698b5ccc187255fe01758dee44cc558e
Amine Herradi <amine.herradi@carventura.com>
2020-05-16 03:12:41 +0200

    AHI - #12 - Database models and model interface

--- Public/index.php
+++ Public/index.php
@@ -7,8 +7,13 @@ include '../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable('/var/www/html/averoes/averoes_users');
 $dotenv->load();
 
+use Models\Exceptions\ModelException;
 use Models\UserEmailModel;
 
 $model = new UserEmailModel();
-$collections = $model->getOneById(6, ['*']);
-var_dump($collections);
+try {
+    $collections = $model->getOneById(6, ['*']);
+    var_dump($collections);
+} catch (ModelException $exception) {
+    echo $exception->getMessage();
+}
